509b4079285dedb8283fba6841010940d6292171
Riya Savant <rieee2110@gmail.com>
2021-10-24 15:56:54 +0530

    Initial Setup

---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent, ReactNode } from "react";
+import styles from "./Box.module.scss";
+
+const Box: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
+  return <div className={styles.Box}>{children}</div>;
+};
+
+export default Box;
