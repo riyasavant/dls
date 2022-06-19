3a67657df2c02dba3275b58735b53d4861c695b3
Syed Shahabaaz Ahmed <46737801+shah9840@users.noreply.github.com>
2022-06-19 17:50:12 +0530

    Add files via upload

---
+++ src/components/StyledImage/StyledImage.stories.tsx
@@ -0,0 +1,12 @@
+import StyledImage from "./StyledImage"
+
+
+export default {
+    title: 'StyledImage'
+};
+
+export const Default = () => {
+    return (
+        <StyledImage url="https://thumbs.dreamstime.com/b/jpg-letter-logo-design-black-background-jpg-creative-initials-letter-logo-concept-jpg-letter-design-jpg-letter-logo-design-243826959.jpg" />
+    )
+}
